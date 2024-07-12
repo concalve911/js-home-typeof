855c2b793171aa4b06c24e99eecee49a57d2c500
concalve911 <aleksandrgarkusha911@gmail.com>
2024-07-12 06:48:35 +0200

    first commit

---
+++ js/script.js
@@ -0,0 +1,24 @@
+"use strict";
+const num = 25;
+console.log(typeof num);
+
+const logic = true;
+console.log(typeof logic);
+
+let userFile;
+console.log(typeof userFile);
+
+const user = null;
+console.log(typeof user);
+
+const obj = {};
+console.log(typeof obj);
+
+const str = "String";
+console.log(typeof str);
+
+const bigNum = 12341154n;
+console.log(typeof bigNum);
+
+const tag = Symbol("tag");
+console.log(typeof tag);
